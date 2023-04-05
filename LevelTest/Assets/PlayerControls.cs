2431db98c369949fee0e05af73ab10919d05fa6e
Hutain <RHutain@ocvts.org>
2023-04-05 01:57:24 -0400

    Added fuel limit + tracker

--- LevelTest/Assets/PlayerControls.cs
+++ LevelTest/Assets/PlayerControls.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControls : MonoBehaviour
 {
@@ -14,21 +15,28 @@ public class PlayerControls : MonoBehaviour
     public float inputX;
     public float inputY;
 
+    public int fuelLevel = 100;
+    public TextMeshProUGUI fuelText;
+
     void Start() {
         status = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         rb = obj.GetComponent<Rigidbody>();
+        fuelText.text = $"Fuel Level: {fuelLevel.ToString()}";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (status.isRunning == true) {
+        if (status.isRunning == true && fuelLevel > 0) {
             inputX = Input.GetAxisRaw("Horizontal");
             inputY = Input.GetAxisRaw("Vertical");
 
             movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
 
             if (inputX != 0 || inputY != 0) {
+                fuelLevel--;
+                fuelText.text = $"Fuel Level: {fuelLevel.ToString()}";
+
                 rb.AddForce(Vector3.Normalize(movement) * moveSpeed, ForceMode.Acceleration);
             }
         }
